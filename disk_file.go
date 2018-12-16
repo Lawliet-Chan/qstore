3fabb61691e0cd82f77d7a80952328235bf64e7f
CrocdileChan <CrocdileChan@github.com>
2018-12-16 17:36:02 +0800

    define disk_file

--- disk_file.go
+++ disk_file.go
@@ -1,24 +1,44 @@
 package qstore
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 type diskFile struct {
-	Number   int
-	startIdx int64
-	endIdx   int64
+	number   int
+	preName  string
+	startIdx uint64
+	endIdx   uint64
 	idxFile  *os.File
 	dataFile *os.File
 }
 
-func newDiskFile() (*diskFile, error) {
-
+func newDiskFile(number int, preName string, startIdx uint64) (*diskFile, error) {
+	name := preName + "-" + strconv.Itoa(number)
+	idxFile, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+	dataFile, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+	return &diskFile{
+		number:   number,
+		preName:  preName,
+		startIdx: startIdx,
+		endIdx:   startIdx,
+		idxFile:  idxFile,
+		dataFile: dataFile,
+	}, nil
 }
 
 func (df *diskFile) writeIdx() {
 
 }
 
-func (df *diskFile) write() {
+func (df *diskFile) write(b []byte) error {
 
 }
 
@@ -27,5 +47,9 @@ func (df *diskFile) readIdx() {
 }
 
 func (df *diskFile) read() {
+
+}
+
+func (df *diskFile) truncate() {
 
 }
